bfe0abd5edab51d8525401723631e3f45757a277
hoaxdream <hoaxdream@gmail.com>
2022-01-15 22:43:00 +0800

    First commit

--- config.h
+++ config.h
@@ -3,7 +3,11 @@
 #define CLICKABLE_BLOCKS 0
 
 const Block blocks[] = {
-	BLOCK("sb-date",    1,    24),
-	// BLOCK("sb-network", 5,    25),
+    BLOCK("sb-mem",     10,     20),
+    BLOCK("sb-gpu",     10,     21),
+    BLOCK("sb-cpu",     10,     22),
+    BLOCK("sb-time",    1,      23),
+    BLOCK("sb-date",    1,      24),
+    // BLOCK("sb-network", 5,    25),
 };
 
